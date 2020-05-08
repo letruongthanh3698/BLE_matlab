d25500c30752148358923c719f222038ad158eaa
letruongthanh24103698 <letruongthanh3698@gmail.com>
2020-05-08 15:44:27 +0700

    2020.05.08:15.44 update report

--- code/static_pathloss.m
+++ code/static_pathloss.m
@@ -1,20 +1,32 @@
-p_exp1=1.75;
-p_exp2=2;
-p_exp3=2.25;
+p_exp1=3;
+p_exp2=3.5;
+p_exp3=4;
+p_exp4=1.75;
+p_exp5=2.0;
+p_exp6=2.25;
 dis_static1=[];
 dis_static2=[];
 dis_static3=[];
+dis_static4=[];
+dis_static5=[];
+dis_static6=[];
 R1=-40;
-p_exp=-(pathloss93-R1)./(10*log10(5.95));
+p_exp59=-(pathloss59-R1)./(10*log10(6.37));
+p_exp93=-(pathloss93-R1)./(10*log10(7.04));
+p_expc2=-(pathlossc2-R1)./(10*log10(8.78));
+p_expc0=-(pathlossc0-R1)./(10*log10(5.41));
 for i=1:1:length(tag93)
     dis_static1(i)=10.^((R1-(tag93(i)))./(10*p_exp1));
     dis_static2(i)=10.^((R1-(tag93(i)))./(10*p_exp2));
     dis_static3(i)=10.^((R1-(tag93(i)))./(10*p_exp3));
+    dis_static4(i)=10.^((R1-(tag93(i)))./(10*p_exp4));
+    dis_static5(i)=10.^((R1-(tag93(i)))./(10*p_exp5));
+    dis_static6(i)=10.^((R1-(tag93(i)))./(10*p_exp6));
 end
 
 figure('Name','Static Pathloss Exponent')
-plot(time,dis_static1,'r',time,dis_static2,'g',time,dis_static3,'b',time,d93,'k')
-legend({'Pathloss Exponent =1.75','Pathloss Exponent =2.00','Pathloss Exponent =2.25','Dynamic Pathloss Exponent'},'FontSize',11,'Location','northoutside');
+plot(time,dis_static4,'r',time,dis_static5,'g',time,dis_static6,'b',time,d93,'k')
+legend({'Pathloss Exponent =3.0','Pathloss Exponent =3.5','Pathloss Exponent =4.0','Dynamic Pathloss Exponent'},'FontSize',11,'Location','northoutside');
 set(gca,'xtick',[t_min:t_step:t_max]) 
 datetick('x','yyyy-mm-dd  HH:MM','keepticks')
 xlabel('Time')
@@ -24,8 +36,19 @@ ax.XTickLabelRotation = 30;
 grid on
 
 figure('Name','Dynamic Pathloss Exponent')
-plot(time,p_exp)
-legend({'Dynamic Pathloss Exponent'},'FontSize',11,'Location','northoutside');
+plot(time,p_exp59,'r',time,p_exp93,'g',time,p_expc2,'b',time,p_expc0,'k')
+legend({'Dynamic Pathloss Exponent TAG59','Dynamic Pathloss Exponent TAG93','Dynamic Pathloss Exponent TAGC2','Dynamic Pathloss Exponent TAGC0'},'FontSize',11,'Location','northoutside');
+set(gca,'xtick',[t_min:t_step:t_max]) 
+datetick('x','yyyy-mm-dd  HH:MM','keepticks')
+xlabel('Time')
+ylabel('Pathloss Exponent')
+ax = gca;
+ax.XTickLabelRotation = 30;
+grid on
+
+figure('Name','Static Pathloss Exponent Custom')
+plot(time,dis_static2,'r',time,d93,'b')
+legend({'Pathloss Exponent = 3.5','Dynamic Pathloss Exponent'},'FontSize',11,'Location','northoutside');
 set(gca,'xtick',[t_min:t_step:t_max]) 
 datetick('x','yyyy-mm-dd  HH:MM','keepticks')
 xlabel('Time')
